bffc3346e4a2785c7a65ee39a42251b2d20367b7
Chris Dunphy <chris@megaparsec.ca>
2024-06-05 22:13:43 -0600

    Install and configure zoxide on Windows

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -10,12 +10,9 @@ Import-Module -Name Terminal-Icons
 # Aliases
 New-Alias -Name vim -Value nvim
 
-# Functions
-function cdot { cd "C:\Users\RCD\Code\Configs\dotfiles" }
-
-function cpy { cd "C:\Users\RCD\Code\Python" }
-
 function which ($command) {
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
+
+Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
